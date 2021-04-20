17f9af00ef3038233b662e66ea5b7e654f746b5d
Thiago <troyom51@gmail.com>
2021-04-20 15:46:13 -0300

    Tarefa

---
+++ NavMeshStationStarter/Assets/AIControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIControl : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    private void Start()
+    {   //Pega componente NavMesh
+        agent = this.GetComponent<NavMeshAgent>();
+    }
+}
---
+++ NavMeshStationStarter/Assets/AgentManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AgentManager : MonoBehaviour
+{
+    GameObject[] agents;
+
+    void Start() { 
+        //Procura o GameObject
+        agents = GameObject.FindGameObjectsWithTag("ai"); 
+    }
+
+    void Update()
+    {   
+        //Ativa ao apertar o botao
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+        { 
+                //Adiciona o destino
+                foreach (GameObject a in agents) a.GetComponent<AIControl>().agent.SetDestination(hit.point); }
+        
+        }
+    }
+}
